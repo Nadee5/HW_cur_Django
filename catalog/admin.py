4f9c2f4e92433eeb1e8b03dca1933be6b780264b
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2023-12-14 19:58:25 +0300

    HW3 (registered Product, Category)

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,16 +5,16 @@ from catalog.models import Product, Category
 
 # Register your models here.
 
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ('pk', 'name', 'price_for_one', 'category',)
-#     list_filter = ('category',)
-#     search_fields = ('name', 'description',)
-#
-#
-# @admin.register(Category)
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ('pk', 'name',)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'price_for_one', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name',)
 
 
 
