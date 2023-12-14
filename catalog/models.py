1e5e9261071836cfa255ac4ac500d9fd29f6f1ea
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2023-12-14 13:47:59 +0300

    HW3 (Product, Category)

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='product/', verbose_name='Изображение', **NULLABLE)
+    category = models.CharField(max_length=50, verbose_name='Категория')
+    price_for_one = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена за штуку')
+    date_of_create = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+    date_of_last_change = models.DateTimeField(auto_now_add=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} ({self.category}) - {self.price_for_one}/шт.'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name} ({self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+
